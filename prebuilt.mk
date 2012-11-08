78495ce1c460aa09142a08dc9840aa601873d9aa
yumingyang <yumingyang@xiaomi.com>
2012-11-08 15:56:06 +0800

    add shelld
    Change-Id: I3613163a5389fe588733b25b42e7216f808ae95b

--- prebuilt.mk
+++ prebuilt.mk
@@ -1,6 +1,7 @@
 add-prebuilt-app: $(ZIP_DIR)/system/xbin/busybox
 	@echo To add prebuilt apps
 	$(hide) cp -f $(SYSOUT_DIR)/xbin/invoke-as $(ZIP_DIR)/system/xbin/
+	$(hide) cp -f $(SYSOUT_DIR)/xbin/shelld $(ZIP_DIR)/system/xbin/
 	$(hide) mkdir -p $(ZIP_DIR)/data/media
 	$(hide) cp -rf $(DATAOUT_DIR)/media/preinstall_apps/ $(ZIP_DIR)/data/media/
 
@@ -52,6 +53,7 @@ release-prebuilt-app:
 	@echo Release prebuilt apps
 	$(hide) mkdir -p $(RELEASE_PATH)/system/xbin
 	$(hide) cp $(SYSOUT_DIR)/xbin/invoke-as $(RELEASE_PATH)/system/xbin/
+	$(hide) cp $(SYSOUT_DIR)/xbin/shelld $(RELEASE_PATH)/system/xbin/
 	$(hide) cp $(SYSOUT_DIR)/xbin/busybox $(RELEASE_PATH)/system/xbin/
 	$(hide) mkdir -p $(RELEASE_PATH)/system/bin
 	$(hide) cp $(SYSOUT_DIR)/bin/installd $(RELEASE_PATH)/system/bin/
