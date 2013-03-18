9a758a5a99fdc9e293fcd310a0ba8f55138acfff
wangwei_b <wangwei_b@xiaomi.com>
2013-03-18 10:25:00 +0800

    add mp3 lib
    Change-Id: Ib6af76670dde01e45da1d3c72847b7bc8af63288
    Signed-off-by: wangwei_b <wangwei_b@xiaomi.com>

--- prebuilt.mk
+++ prebuilt.mk
@@ -20,6 +20,7 @@ add-prebuilt-libraries:
 	$(hide) cp -f $(SYSOUT_DIR)/lib/libimageutilities_jni.so $(ZIP_DIR)/system/lib/
 	$(hide) cp -f $(SYSOUT_DIR)/lib/libpatcher_jni.so $(ZIP_DIR)/system/lib/
 	$(hide) cp -f $(SYSOUT_DIR)/fonts/TobysHand.ttf $(ZIP_DIR)/system/fonts/
+	$(hide) cp -f $(SYSOUT_DIR)/lib/libmp3lame.so $(ZIP_DIR)/system/lib/
 
 
 add-prebuilt-media:
@@ -78,6 +79,7 @@ release-prebuilt-libraries:
 	$(hide) cp -f $(SYSOUT_DIR)/lib/libphotocli.so $(RELEASE_PATH)/system/lib/
 	$(hide) cp -f $(SYSOUT_DIR)/lib/libimageutilities_jni.so $(RELEASE_PATH)/system/lib/
 	$(hide) cp -f $(SYSOUT_DIR)/lib/libpatcher_jni.so $(RELEASE_PATH)/system/lib/
+	$(hide) cp -f $(SYSOUT_DIR)/lib/libmp3lame.so $(ZIP_DIR)/system/lib/
 
 release-prebuilt-media:
 	@echo Release prebuilt media files
