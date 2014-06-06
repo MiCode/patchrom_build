08470f13a7c03a9056840be799684805ac26e823
mochangming <mochangming@xiaomi.com>
2014-06-06 15:19:07 +0800

    adjust miui apps
    Change-Id: Ife5c2b99f48adfc9490ece8590092c998818786c

--- kitkat.mk
+++ kitkat.mk
@@ -1,65 +1,69 @@
-private-miui-priv-apps := 		 Backup  \
-					 Calendar  \
-					 Contacts  \
-					 DownloadProvider  \
-					 FusedLocation \
-					 MiuiGallery  \
-					 MediaProvider  \
-					 MiuiHome  \
-                                         MiuiKeyguard \
-					 MiuiSystemUI  \
-					 Mms  \
-					 Music  \
-					 Phone  \
-					 QuickSearchBox  \
-					 Settings  \
-					 SettingsProvider  \
-					 VpnDialogs \
-					 YellowPage 
-private-miui-apps := AntiSpam  \
-                                         AirkanPhoneService \
-					 BaiduNetworkLocation  \
-					 Browser  \
-					 BugReport  \
-					 Calculator  \
-					 CalendarProvider  \
-					 CloudService  \
-					 ContactsProvider  \
-					 DataHubProvider \
-					 DeskClock  \
-					 DownloadProviderUi  \
-					 Email  \
-					 FileExplorer  \
-					 GameCenter \
-					 GameCenterSDKService \
-					 GuardProvider \
-					 KingSoftCleaner \
-					 LBESEC_MIUI \
-					 LiveWallpapersPicker \
-					 MiWallpaper \
-					 MiuiCompass  \
-					 MiuiVideo \
-					 NetworkAssistant2 \
-					 Notes  \
-					 PackageInstaller  \
-					 PaymentService \
-					 Provision \
-					 SoundRecorder  \
-					 SuperMarket  \
-					 TelephonyProvider  \
-					 ThemeManager  \
-					 Transfer \
-					 Updater \
-					 Weather \
-					 WeatherProvider \
-					 XiaomiServiceFramework \
-					 VoiceAssist \
+private-miui-priv-apps := Backup  \
+                     Calendar  \
+                     Contacts  \
+                     DownloadProvider  \
+                     FusedLocation \
+                     MiuiGallery  \
+                     MediaProvider  \
+                     MiuiHome  \
+                     MiuiKeyguard \
+                     MiuiSystemUI  \
+                     Mms  \
+                     Music  \
+                     Phone  \
+                     QuickSearchBox  \
+                     Settings  \
+                     SettingsProvider  \
+                     VpnDialogs \
+                     YellowPage
 
-private-preinstall-apps := MiShop \
-					 Userbook \
+
+private-miui-apps := AntiSpam  \
+                     AirkanPhoneService \
+                     BaiduNetworkLocation  \
+                     Browser  \
+                     BugReport  \
+                     Calculator  \
+                     CalendarProvider  \
+                     CloudService  \
+                     ContactsProvider  \
+                     DataHubProvider \
+                     DeskClock  \
+                     DownloadProviderUi  \
+                     Email  \
+                     FileExplorer  \
+                     GameCenter \
+                     GameCenterSDKService \
+                     GuardProvider \
+                     KingSoftCleaner \
+                     LBESEC_MIUI \
+                     LiveWallpapersPicker \
+                     MiLinkService \
+                     MiWallpaper \
+                     MiuiCompass  \
+                     MiuiVideo \
+                     NetworkAssistant2 \
+                     Notes  \
+                     PackageInstaller  \
+                     PaymentService \
+                     Provision \
+                     SoundRecorder  \
+                     SuperMarket  \
+                     TelephonyProvider  \
+                     ThemeManager  \
+                     Updater \
+                     Weather \
+                     WeatherProvider \
+                     XiaomiServiceFramework \
+                     VoiceAssist
+
+private-preinstall-apps := ota-miui-MiShop \
+                     ota-partner-XunfeiSpeechService3 \
+                     ota-partner-NetworkAssistant
+
 
 private-miui-jars := services \
-					 android.policy \
-					 telephony-common \
-					 framework \
-					 framework2 \
+                     android.policy \
+                     telephony-common \
+                     framework \
+                     framework2
